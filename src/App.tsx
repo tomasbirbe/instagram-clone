1c5d5ca852bd2b7769e5752aa83939811114efe1
Tomas Birbe <tomas.birbe@gmail.com>
2022-11-27 12:10:24 -0300

    main: init project and create a basic structure for Home

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+export default function App() {
+  return <>
+  <nav>
+        <img />
+        <ul>
+          <li>Inicio</li>
+          <li>Buscar</li>
+          <li>Explorar</li>
+          <li>Mensajes</li>
+          <li>Notificaciones</li>
+          <li>Crear</li>
+          <li>Perfil</li>
+        </ul>
+        <button />
+      </nav>
+      <main>
+        <div>Historias</div>
+        <div>Feed</div>
+      </main>
+      <aside>
+        <div>Mi perfil</div>
+        <div>Sugerencias</div>
+      </aside>
+    </>
+}
